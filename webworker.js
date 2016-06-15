fc4c8a65824b49b2be6ced4d6e93af377d294278
eterna2 <eterna2@hotmail.com>
2016-06-16 02:06:32 +0800

    Added more function

--- webworker.js
+++ webworker.js
@@ -1,15 +1,18 @@
+importScripts('./bower_components/d3/d3.min.js');
+importScripts('./bower_components/lodash/lodash.min.js');
+
+var ctx = Object.create(null);
+
 onmessage = function(e) {
   run(e.data);
 }
 
 function run(msg){
   var res;
-  console.log(msg)
   try{
-    var code = msg.code.replace(/\bvar\b/gmi,'this.');
-    res = eval(code);
-    res = res ? JSON.stringify(res):'';
-
+    var code = msg.code.replace(/\bvar\b/gmi,'ctx.');
+    res = eval(genReply(msg.id)+code);
+    res = res?JSON.stringify(res):res;
   }catch(err){
     console.log(err);
     res = err.toString();
@@ -17,4 +20,8 @@ function run(msg){
     postMessage({id:msg.id, res:res});
 
   }
+}
+
+function genReply(id){
+  return 'var print=function(res){postMessage({id:'+id+', res:JSON.stringify(res)});};';
 }
